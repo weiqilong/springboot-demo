a0570bf7801ef9ac79edb90d65a30f376191339b
weiqilong <weiqilong@aspirecn.com>
2018-11-05 15:50:17 +0800

    第一次

---
+++ lean-springboot-shiro/src/main/java/com/wql/App.java
@@ -0,0 +1,20 @@
+package com.wql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * 入口
+ * @author weiqilong
+ * CREATE TIME 2018年11月5日 下午2:45:40
+ */
+@SpringBootApplication
+@EnableTransactionManagement
+@ComponentScan(basePackages = { "com.wql" })
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class,args);
+	}
+}

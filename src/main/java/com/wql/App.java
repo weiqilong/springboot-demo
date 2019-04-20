9739003d2f16942dbcc58cbd9cfe56164fa440cd
weiqilong <weiqilong@aspirecn.com>
2019-04-20 19:08:12 +0800

    注释掉app类里的componentScan注解

--- src/main/java/com/wql/App.java
+++ src/main/java/com/wql/App.java
@@ -2,7 +2,6 @@ package com.wql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 

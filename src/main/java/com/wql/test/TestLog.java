08c31bf956adb1b04a2f6d5eef6da98d9d6b63fe
小熊羔子 <41141563+weiqilong@users.noreply.github.com>
2019-04-20 12:36:54 +0800

    git修改3

--- src/main/java/com/wql/test/TestLog.java
+++ src/main/java/com/wql/test/TestLog.java
@@ -5,7 +5,7 @@ import org.apache.logging.log4j.Logger;
 
 /**
  * a b
- * master master xxxxxxx
+ * master master fffffff
  */
 public class TestLog {
 	private static final Logger logger = LogManager.getLogger(TestLog.class);

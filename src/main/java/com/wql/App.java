18a031d53fac00b65f1e649549ac12ffa81489a5
weiqilong <weiqilong@aspirecn.com>
2019-04-24 23:03:22 +0800

    修改描述

--- src/main/java/com/wql/App.java
+++ src/main/java/com/wql/App.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * 下次记得写aop拦截计算方法耗时 拦截注解
  * 记得写拦截器 模拟拦截验签
  * 记得添加校验入参工具
+ * 记得测试是否能连通数据库
  * @author weiqilong
  * CREATE TIME 2018年11月5日 下午2:45:40
  */
